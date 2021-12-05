081a1e7b2fd2817d4d7179b1045e11124ab7e6f9
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2021-12-05 13:58:27 +0300

    .

---
+++ DegerveReferansTipler/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            Console.WriteLine(sayi1);
+
+            int[] sayiler1 = new int[] { 10, 20, 30 };
+            int[] sayiler2 = new int[] { 100, 200, 300 };
+            sayiler1 = sayiler2;
+            sayiler2[0] = 999;
+            Console.WriteLine(sayiler1[0]);
+        }
+    }
+}
