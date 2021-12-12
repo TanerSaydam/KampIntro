022878a13d0896df4f6de7a1991bed0ab205ed3e
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2021-12-12 21:23:26 +0300

    12.12.2021 Ders 5 Sonu

---
+++ OOP3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IhtiyacKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            TasitKrediManager tasitKrediManager = new TasitKrediManager();
+            KonutKrediManager konutKrediManager = new KonutKrediManager();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(new EsnafKrediManager(), new List<ILoggerService>() { new DatabaseLoggerService(), new SmsLoggerService()});
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, tasitKrediManager,konutKrediManager};
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+            Console.ReadLine();
+        }
+    }
+}
